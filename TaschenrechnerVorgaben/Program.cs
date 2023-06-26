51dbecb35b4fd000a3603cf62f8dedf34f720499
Lilo1417 <linoallg@schafferts.ch>
2023-06-26 16:58:00 +0200

    Projektdateien hinzufügen.

---
+++ TaschenrechnerVorgaben/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Transactions;
+using TaschenrechnerVorgaben;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+            string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
+            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein (+, -, /, *): ");
+
+            // Wandel Text in Gleikommazahlen
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
+            double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
+
+            // Berechnung ausführen
+            RechnerModel model = new RechnerModel();
+            double resultat = model.Berechne(ersteZahl, zweiteZahl, operation);
+
+            // Ausgabe
+            GibResultatAus(resultat, operation);
+            HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
+        }
+
+        static string HoleBenutzerEingabe(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            return Console.ReadLine();
+        }
+
+
+        static void GibResultatAus(double resultat, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Summe ist: {0}", resultat);
+                    break;
+
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
+
+                case "/":
+                    Console.WriteLine("Der Wert des Quotienten ist: {0}", resultat);
+                    break;
+
+                case "*":
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
+                    break;
+            }
+        }
+    }
+}
